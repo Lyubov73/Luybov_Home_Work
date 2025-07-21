878558d6751e336370f4ca3e14eb144b2cb04c4c
Lybov73 <baimetovalyubov1822@yandex.ru>
2025-07-21 22:04:08 +0300

    lesson2

--- 02_lesson/lesson_2_task_3.py
+++ 02_lesson/lesson_2_task_3.py
@@ -1,8 +1,9 @@
+import math
 def square(side):
-area = side * side 
-if not isinstance(side,int): 
-    area = match.ceil(area) 
-    return area 
-print(square(4))
-print(square(4.6))
-print(square(5.9))
+    are = side * side
+    if not isinstance(side,int):
+        are = math.ceil(are)
+        return are
+    print(square(4))
+    print(square(4.6))
+    print(square(5.9))
