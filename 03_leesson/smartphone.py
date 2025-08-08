d0dd32704179d7f8d2b1f5d40b50469edb435a41
Lybov73 <baimetovalyubov1822@yandex.ru>
2025-08-08 15:05:09 +0300

    сматрфоны

--- 03_leesson/smartphone.py
+++ 03_leesson/smartphone.py
@@ -1,1 +1,10 @@
-class Smartphone
+class Smartphone:
+ def  __init__(self,brand,model,numer):
+  self.brand = brand
+  self.model = model
+  self.numer = numer
+catalog = []
+Smartphone("Apple","iPhone 14","+79172345678"),
+Smartphone("Samsung","Galaxy S40","+79127354379"),
+Smartphone("Xiaomi","Redmi Note 13","+79053677358"),
+Smartphone("")
