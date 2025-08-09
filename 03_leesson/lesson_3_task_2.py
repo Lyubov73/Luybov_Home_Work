e57f002a579b6ad8b19f147913187f47d3b18113
Lybov73 <baimetovalyubov1822@yandex.ru>
2025-08-09 07:29:17 +0300

    lesson3_task_2.py

--- 03_leesson/lesson_3_task_2.py
+++ 03_leesson/lesson_3_task_2.py
@@ -1,1 +1,12 @@
+from smartphone import Smartphone
+
+catalog = [
+Smartphone("Apple","iPhone 14","+79172345678"),
+Smartphone("Samsung","Galaxy S40","+79127354379"),
+Smartphone("Xiaomi","Redmi Note 13","+79053677358"),
+Smartphone("Google","Pixel 6","+79156385279"),
+Smartphone("OnePlus","11 Pro","+79273660487")]
+
+for phone in catalog:
+ print(f"{phone.brand}-{phone.model}.{phone.number}")
 
--- 03_leesson/smartphone.py
+++ 03_leesson/smartphone.py
@@ -1,10 +1,14 @@
 class Smartphone:
- def  __init__(self,brand,model,numer):
+ def  __init__(self,brand,model,number):
   self.brand = brand
   self.model = model
-  self.numer = numer
-catalog = []
+  self.number = number
+catalog = [
 Smartphone("Apple","iPhone 14","+79172345678"),
 Smartphone("Samsung","Galaxy S40","+79127354379"),
 Smartphone("Xiaomi","Redmi Note 13","+79053677358"),
-Smartphone("")
+Smartphone("Google","Pixel 6","+79156385279"),
+Smartphone("OnePlus","11 Pro","+79273660487")]
+
+for phone in catalog:
+ print(f"{phone.brand}-{phone.model}.{phone.number}")
