737a45906667b3151c3db092b877bbc4375e300f
Lybov73 <baimetovalyubov1822@yandex.ru>
2025-08-19 20:19:45 +0300

    03_lesson

--- 03_leesson/lesson_3_task_1.py
+++ 03_leesson/lesson_3_task_1.py
@@ -1,9 +1,9 @@
 from user import User 
 
-Любовь = User("Любовь","Байметова")
-Байметова = User("Любовь","Байметова")
-Любовь_Байметова = User("Любовь","Байметова")
+name = User("Любовь")
+famely = User("Байметова")
+full_name = User("Любовь","Байметова")
 
-Любовь.print_first_name()
-Байметова.print_last_name()
-Любовь_Байметова.print_ful_name()
+name.print_first_name()
+famely.print_last_name()
+full_name.print_ful_name()
